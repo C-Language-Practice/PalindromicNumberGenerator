bee75a844291f4f73d97e9f76626f5e2ff08366f
Krisz Kaszas <kriszrokakaszas@gmail.com>
2020-12-06 13:58:29 +0100

    done

---
+++ PalindromicNumberGenerator/PalindromicNumberGenerator/PalindromicNumberGenerator.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int end = 0;
+	int	mid = 0;
+	
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			printf_s("%d%d%d\n", end, mid, end);
+			mid++;
+		}
+		mid = 0;
+		end++;
+	}
+	return 0;
+}
